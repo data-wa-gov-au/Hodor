bb3566d141e60ab7cc2b3775ea8f43a3045c873a
Keith Moss <keith.moss@landgate.wa.gov.au>
2014-07-20 17:12:10 +0800

    Moving back to streaming for uploads
    Moving back to streaming for uploads now that we’ve fixed the TCP send
    buffer issue on Windows.

--- hodor/commands/cmd_create.py
+++ hodor/commands/cmd_create.py
@@ -9,7 +9,7 @@ from hodor.cli import pass_context
 
 @click.group(short_help="Create assets in Google Maps Engine")
 # https://google-api-python-client.googlecode.com/hg/docs/epy/apiclient.http.MediaFileUpload-class.html
-@click.option('--chunk-size', default=-1,
+@click.option('--chunk-size', default=10485760,
               help='File chunk size in bytes. Defaults to using streaming to send files.')
 @click.option('--asset-processing-wait', default=90,
               help='Length of time in minutes we should wait for an asset to process before giving up. Defaults to 90 minutes.')
