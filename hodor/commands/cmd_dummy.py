7bf806ad259acad4dd5298ccb102e6da55cda618
Keith Moss <keith.moss@landgate.wa.gov.au>
2014-07-27 11:04:40 +0800

    Adding a once-off script to tag and untag all rasters
    Adding a once-off script to tag and untag all rasters to work around
    the backend GME bug with the new ACL system.

--- hodor/commands/cmd_dummy.py
+++ hodor/commands/cmd_dummy.py
@@ -2,6 +2,7 @@ import click
 import os
 import json
 import random
+from pprintpp import pprint as pp
 from retries import retries
 from hodor.cli import pass_context
 
@@ -9,6 +10,45 @@ from hodor.cli import pass_context
 @pass_context
 def cli(ctx):
   pass
+
+
+@cli.command()
+@pass_context
+def tag_all_rasters(ctx):
+  import httplib2
+  h = httplib2.Http()
+  request_uri = "https://www.googleapis.com/mapsengine/exp2/rasters?projectId=09372590152434720789&fields=nextPageToken,rasters/id,rasters/tags"
+  next_page_token = ""
+
+  while next_page_token is not None:
+    response, content = h.request(
+      request_uri + "&pageToken=" + next_page_token, "GET",
+      headers={
+        "Authorization": "Bearer " + ctx.access_token,
+        "Content-type": "application/json"
+      }
+    )
+
+    if response['status'] == "200":
+      content = json.loads(content)
+      if 'nextPageToken' in content:
+        next_page_token = content['nextPageToken']
+
+        # Tag and untag all rasters to trigger GME to switch them to the new ACL system
+        for r in content['rasters']:
+          patch = ctx.service.rasters().patch(id=r['id'], body={
+            "tags": r['tags'] + ["hodor-patch"]
+          }).execute()
+          patch = ctx.service.rasters().patch(id=r['id'], body={
+            "tags": r['tags']
+          }).execute()
+          ctx.log("%s patched OK" % (r['id']))
+
+      else:
+        next_page_token = None
+    else:
+      raise Exception("Got a non-200 response")
+
 
 @cli.command()
 @click.option('--table-id', type=str)
