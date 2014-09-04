2646f89a9c8a64d77219e5d2624f254368789caa
Keith Moss <keith.moss@landgate.wa.gov.au>
2014-09-04 16:28:23 +0800

    Faux Table Replace plumbed together
    For issue #44

--- hodor/commands/cmd_faux-table-replace.py
+++ hodor/commands/cmd_faux-table-replace.py
@@ -16,11 +16,9 @@ def cli(ctx):
 
 
 @cli.command()
-# @TOOO Make this click.File afterall. Intention being that AE has a bunch of .bat files at the top level
-# that he points at individual jobfiles and > pipes output to a /logs dir
-@click.argument('jobstore', type=click.Path(exists=True, file_okay=False, resolve_path=True))
+@click.argument('configfile', type=click.File())
 @pass_context
-def runjob(ctx, jobstore):
+def runjob(ctx, configfile):
   """
     And on the pedestal these words appear:
     'My name is Ozymandias, king of kings:
@@ -31,126 +29,149 @@ def runjob(ctx, jobstore):
       http://en.wikipedia.org/wiki/Ozymandias
   """
 
-  config = {
-    "projectId": "09372590152434720789",
-    "custodian" : "lgate",
-    "title" : "admin_lga",
-    "dateStamp" : "{Date}"
-    # "partNumber" : "1",
-    # "partCount" : "2"
-  }
+  config = json.load(configfile)
 
   datasource_name_part = config["title"] + "_" + config['custodian']
   if "partNumber" in config and "partCount" in config:
     datasource_name_part += "_" + config["partNumber"] + "_of_" + config["partCount"]
 
-  h = httplib2.Http()
-  resp, content = h.request(
-    "https://www.googleapis.com/mapsengine/search_tt/assets?type=table&search=" +\
-        quote_plus((datasource_name_part.replace("_", " "))) + "&projectId=" + config["projectId"], "GET",
-    headers={
-      "Authorization": "Bearer " + ctx.access_token
-    }
-  )
+  response = ctx.service.tables().list(projectId=config["projectId"], search=datasource_name_part.replace("_", " ")).execute()
 
-  table = None
-  if resp['status'] != "200":
-    raise Exception("Received status '" + resp['status'] + "' from search. Gave up.")
+  valid_tables = []
+  for t in response["tables"]:
+    m = re.search("^(" + datasource_name_part + "_[0-9]{8})$", t["name"])
+    if m and "archive" not in t["tags"] and t["processingStatus"] == "complete":
+      valid_tables.append(t)
+
+  if len(valid_tables) != 1:
+    raise Exception("Found " + str(len(valid_tables)) + " matching tables. Gave up - we require a single matching table.")
   else:
-    valid_tables = []
-    for t in json.loads(content)["assets"]:
-      m = re.search("^(" + datasource_name_part + "_[0-9]{8})$", t["name"])
-      if m and "archive" not in t["tags"] and t["processingStatus"] == "complete":
-        valid_tables.append(t)
-        pp(t)
+    table = valid_tables[0]
+    layers = ctx.service.tables().parents().list(id=table["id"]).execute()["parents"]
+    ctx.log("Found table %s (%s)" % (table["name"], table["id"]))
 
-    if len(valid_tables) != 1:
-      raise Exception("Found " + str(len(valid_tables)) + " matching tables. Gave up - we require a single matching table.")
-    else:
-      table = valid_tables[0]
+    # Step 1 - Create a temporary table.
+    ctx.log("### Create Temporary Table")
+    temp_table_id = createTemporaryTable(ctx, table)
 
-    # Step 1 - Create an empty placeholder table with the same schema
-    createPlaceholderTable(ctx, table)
+    # Step 2 - Point our target table's layers at the temporary table.
+    ctx.log("### Patch Layers")
+    patchLayers(ctx, layers, temp_table_id)
+
+    # Step 3 - Upload our payload files to replace the data in our table.
+    ctx.log("### Replace Files")
+    replaceFiles(ctx, table["id"], os.path.join(os.path.dirname(configfile.name), "payload"))
+
+    # Step 4 - Point our layers back at the proper table.
+    ctx.log("### Patch Layers")
+    patchLayers(ctx, layers, table["id"])
+
+    # Step 5 - Delete the temporary table.
+    ctx.log("### Delete Temporary Table")
+    deleteTable(ctx, temp_table_id)
+
+    # Step 6 - Reprocess and republish the layers.
+    ctx.log("### Reprocess & Republish Layers")
+    reprocessAndRepublishLayers(ctx, layers)
+
+    ctx.log("### Faux Table Replace Complete. Have a nice day.")
 
 
+def createTemporaryTable(ctx, table):
+  """Create an empty table with a schema
+  matching the table provided.
 
-def createPlaceholderTable(ctx, table):
+  Parameters
+  ----------
+  ctx : Context
+    A Click Context object.
+  table : dict
+    A GME Table object.
+
+  Returns
+  -------
+  int
+    The Id of the temporary table.
+  """
+
   @retries(10)
   def get_table_schema(table):
     return ctx.service.tables().get(id=table['id'], fields="schema").execute()["schema"]
 
   @retries(10)
   def create_table(config):
-    pp(config)
     return ctx.service.tables().create(body=config).execute()
 
   schema = get_table_schema(table)
 
-  placeholder_table = create_table({
+  table = create_table({
     "projectId": table["projectId"],
-    "name": table["name"] + "_placeholder",
-    "draftAccessList": "Map Editors",
+    "name": table["name"] + "_ftrtemp",
     "schema": {
       "columns": [v for v in schema["columns"] if v["name"] != "gx_id"],
       "primaryKey": schema["primaryKey"] if schema["primaryKey"]  != "gx_id" else None
     }
   })
-  ctx.log("Created empty placeholder table " + placeholder_table["id"])
+  ctx.log("Created temporary table " + table["id"])
+  return table["id"]
 
 
+def patchLayers(ctx, layers, table_id):
+  """Perform a patch operation on the layers associated
+  with a table to point them at another table.
+
+  Parameters
+  ----------
+  ctx : Context
+    A Click Context object.
+  layers : list
+    The Ids of the layers to patch.
+  table_id : int
+    The Id of the table the layers will point to.
+  """
+
+  @retries(10)
+  def patch(ctx, layer_id, body):
+    return ctx.service_exp2.layers().patch(id=layer_id, body=body).execute()
+
+  for l in layers:
+    patch(ctx, l["id"], {
+      "datasources": [
+        {"id": table_id}
+      ]
+    })
+    ctx.log("Layer %s successfully patched." % (l["id"]))
 
 
-@cli.command()
-@click.argument('layer-id', type=str)
-@click.argument('table-id', type=str)
-@pass_context
-def patch(ctx, layer_id, table_id):
-  patch = {
-    "datasources": [
-      {"id": table_id}
-    ]
-  }
-  h = httplib2.Http()
+def replaceFiles(ctx, table_id, payload_dir):
+  """Reupload a given set of files to an existing table for reprocessing.
 
-  resp, content = h.request(
-    "https://www.googleapis.com/mapsengine/exp2/layers/" + layer_id, "PATCH",
-    body=json.dumps(patch),
-    headers={
-      "Authorization": "Bearer " + ctx.access_token,
-      "Content-type": "application/json"
-    }
-  )
+  Parameters
+  ----------
+  ctx : Context
+    A Click Context object.
+  table_id : int
+    The Id of the table to upload to.
+  payload_dir : str
+    The path of the payload directory containing the files.
+  """
 
-  if resp["status"] != "204":
-    raise Exception("Failed patching layer " + layer_id)
-  else:
-    ctx.log("Layer successfully patched.")
+  # @TODO Generic timing code
+  # @TODO Generic polling decerator
 
-
-@cli.command()
-@click.option('--chunk-size', default=10485760,
-              help='File chunk size in bytes. Defaults to using streaming to send files.')
-@click.option('--asset-processing-wait', default=90,
-              help='Length of time in minutes we should wait for an asset to process before giving up. Defaults to 90 minutes.')
-@click.argument('table-id', type=str)
-@click.argument('payload-dir', type=click.Path(exists=True, file_okay=False))
-@pass_context
-def replace(ctx, table_id, payload_dir, chunk_size, asset_processing_wait):
-  ctx.chunk_size = chunk_size
-  ctx.processing_timeout_mins = asset_processing_wait
-
-  @retries((ctx.processing_timeout_mins * 60) / 10, delay=10, backoff=1)
+  @retries((180 * 60) / 10, delay=10, backoff=1)
   def poll_asset_processing(ctx, resource, assetId):
     response = resource.get(id=assetId).execute()
     if response['processingStatus'] in ['complete', 'failed']:
       return response
     else:
-      raise Exception("Asset processing status is '%s'" % (response["processingStatus"]))
+      raise Exception("Table processing status is '%s'" % (response["processingStatus"]))
 
   # Fetch the payload files
   config = {}
   for (dirpath, dirnames, filenames) in os.walk(payload_dir):
-    config['files'] = [{'filename': i} for i in filenames]
+    config['files'] = [{'filename': f} for f in filenames if f != ".DS_Store"]
+    break
 
   # Upload the payload files
   start_time = time.time()
@@ -163,59 +184,81 @@ def replace(ctx, table_id, payload_dir, chunk_size, asset_processing_wait):
   response = poll_asset_processing(ctx, ctx.service.tables(), table_id)
   if response["processingStatus"] == "complete":
     ctx.log("Processing complete and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
-    return response
   elif response["processingStatus"] == "failed":
     ctx.vlog("Processing failed and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
-    raise Exception("Asset failed to process")
+    raise Exception("Table failed to process.")
 
 
-@cli.command()
-@click.argument('layer-id', type=str)
-@pass_context
-def reprocess_layer(ctx, layer_id):
+@retries(10)
+def deleteTable(ctx, table_id):
+  """Deletes a given table.
+
+  Parameters
+  ----------
+  ctx : Context
+    A Click Context object.
+  table_id : int
+    The Id of the table.
+  """
+
+  ctx.service.tables().delete(id=table_id).execute()
+  ctx.log("Table %s successfully deleted." % (table_id))
+
+
+def reprocessAndRepublishLayers(ctx, layers):
+  """Reprocess and republish the given set of layers.
+
+  Parameters
+  ----------
+  ctx : Context
+    A Click Context object.
+  layers : list
+    The Ids of the layers to patch.
+  """
+
+  @retries(100, delay=5)
+  def processLayer(ctx, layer_id):
+    return ctx.service.layers().process(id=layer_id).execute()
+
+  @retries(100, delay=5)
+  def publishLayer(ctx, layer_id):
+    return ctx.service.layers().publish(id=layer_id).execute()
+
   @retries(100, delay=10, backoff=1)
   def poll_asset_processing(ctx, resource, assetId):
     response = resource.get(id=assetId).execute()
     if response['processingStatus'] in ['complete', 'failed']:
       return response
     else:
-      raise Exception("Asset processing status is '%s'" % (response["processingStatus"]))
+      raise Exception("Layer processing status is '%s'" % (response["processingStatus"]))
 
-  ctx.service.layers().process(id=layer_id).execute()
-  ctx.log("Layer processesing begun")
-
-  # Poll until asset has processed
-  start_time = time.time()
-  response = poll_asset_processing(ctx, ctx.service.layers(), layer_id)
-  if response["processingStatus"] == "complete":
-    ctx.log("Processing complete and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
-    return response
-  elif response["processingStatus"] == "failed":
-    ctx.vlog("Processing failed and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
-    raise Exception("Asset failed to process")
-
-
-@cli.command()
-@click.argument('layer-id', type=str)
-@pass_context
-def republish_layer(ctx, layer_id):
   @retries(100, delay=10, backoff=1)
-  def poll_asset_processing(ctx, resource, assetId):
+  def poll_asset_publishing(ctx, resource, assetId):
     response = resource.get(id=assetId).execute()
-    if response['processingStatus'] in ['complete', 'failed']:
+    if response['publishingStatus'] == 'published':
       return response
     else:
-      raise Exception("Asset processing status is '%s'" % (response["processingStatus"]))
+      raise Exception("Layer publishing status is '%s'" % (response["publishingStatus"]))
 
-  ctx.service.layers().publish(id=layer_id).execute()
-  ctx.log("Layer publishing begun")
 
-  # Poll until asset has processed
-  start_time = time.time()
-  response = poll_asset_processing(ctx, ctx.service.layers(), layer_id)
-  if response["processingStatus"] == "complete":
-    ctx.log("Processing complete and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
-    return response
-  elif response["processingStatus"] == "failed":
-    ctx.vlog("Processing failed and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
-    raise Exception("Asset failed to process")
+  for l in layers:
+    processLayer(ctx, l["id"])
+    ctx.log("Layer %s processesing begun." % (l["id"]))
+
+    # Poll until asset has processed
+    start_time = time.time()
+    response = poll_asset_processing(ctx, ctx.service.layers(), l["id"])
+    if response["processingStatus"] == "complete":
+      ctx.log("Processing complete and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
+
+      publishLayer(ctx, l["id"])
+      ctx.log("Layer %s publishing begun." % (l["id"]))
+
+      # Poll until asset has published
+      start_time = time.time()
+      response = poll_asset_publishing(ctx, ctx.service.layers(), l["id"])
+      ctx.log("Publishing complete and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
+
+    elif response["processingStatus"] == "failed":
+      ctx.vlog("Processing failed and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
+      raise Exception("Layer failed to process")
