2001c67b61f72ed6ad47362bee7c949de9916f5e
Keith Moss <keith.moss@landgate.wa.gov.au>
2014-09-05 14:09:42 +0800

    Refactoring asset creation to use the new file upload code

--- hodor/commands/cmd_create.py
+++ hodor/commands/cmd_create.py
@@ -2,6 +2,7 @@ import click
 import json
 import os
 import time
+from hodor.gme import upload_file
 from pprintpp import pprint as pp
 from os import walk
 from retries import retries
@@ -10,7 +11,7 @@ from hodor.cli import pass_context
 @click.group(short_help="Create assets in Google Maps Engine")
 # https://google-api-python-client.googlecode.com/hg/docs/epy/apiclient.http.MediaFileUpload-class.html
 @click.option('--chunk-size', default=10485760,
-              help='File chunk size in bytes. Defaults to using streaming to send files.')
+              help='File chunk size in bytes. Defaults to chunks of 10mb. Pass -1 to use native streaming in Python instead.')
 @click.option('--asset-processing-wait', default=90,
               help='Length of time in minutes we should wait for an asset to process before giving up. Defaults to 90 minutes.')
 @pass_context
@@ -39,7 +40,7 @@ def raster(ctx, mosaic_id, process_mosaic, layer_id, configfile):
       raise Exception("Asset processing status is '%s'" % (response["processingStatus"]))
 
   # Create new raster asset
-  asset = uploader(ctx, ctx.service.rasters(), configfile)
+  asset = uploader(ctx, "raster", ctx.service.rasters(), configfile)
 
   # Optionally, add it to an existing raster mosaic
   if mosaic_id is not None:
@@ -82,13 +83,13 @@ def raster(ctx, mosaic_id, process_mosaic, layer_id, configfile):
 @pass_context
 def vector(ctx, layer_configfile, configfile):
   """Create a new vector asset in Google Maps Engine"""
-  asset = uploader(ctx, ctx.service.tables(), configfile)
+  asset = uploader(ctx, "vector", ctx.service.tables(), configfile)
 
   if layer_configfile:
     create_vector_layers(ctx, asset["id"], layer_configfile)
 
 
-def uploader(ctx, resource, configfile):
+def uploader(ctx, asset_type, resource, configfile):
   @retries(10)
   def create_asset(resource, config):
     return resource.upload(body=config).execute()
@@ -104,7 +105,7 @@ def uploader(ctx, resource, configfile):
   config = json.load(configfile)
 
   # Fetch payload files
-  payloaddir = os.path.join(os.path.dirname(configfile.name), "payload")
+  payloaddir = os.path.join(os.path.dirname(os.path.abspath(configfile.name)), "payload")
   if os.path.isdir(payloaddir):
     for (dirpath, dirnames, filenames) in walk(payloaddir):
       config['files'] = [{'filename': f} for f in filenames if f != ".DS_Store"]
@@ -113,14 +114,14 @@ def uploader(ctx, resource, configfile):
   # Backwards compatibility for Aaron who was supplying the files in the config already
     payloaddir = os.path.dirname(configfile.name)
 
-  # Create asset and upload payload files
+  # Create skeleton table asset
   response = create_asset(resource, config)
   ctx.log("Table '%s' created with id %s" % (response['name'], response['id']))
 
   # Upload the payload files
   start_time = time.time()
   for i in config['files']:
-    ctx.upload_file(os.path.join(payloaddir, i['filename']), response['id'], resource)
+    upload_file(ctx, response['id'], asset_type, os.path.join(payloaddir, i['filename']), ctx.chunk_size)
   ctx.log("All uploads completed and took %s minutes" % (round((time.time() - start_time) / 60, 2)))
 
   # Poll until asset has processed
